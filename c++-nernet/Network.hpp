7308df392f6d8430a40aaf14eebebe4531503a2e
giraf-fe <71472655+giraf-fe@users.noreply.github.com>
2023-11-28 15:19:14 -0800

    make thing

---
+++ c++-nernet/Network.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+namespace ccl {
+
+	class NetworkLayer {
+
+	};
+
+	class NetworkBlueprint {
+	private:
+
+
+	public:
+
+	};
+
+	class NetworkBlueprintBuilder {
+	public:
+		NetworkBlueprintBuilder();
+
+	};
+
+
+}
